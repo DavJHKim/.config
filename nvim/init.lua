a1ba248535a5a691172bff83e031651973145a86
David Ji Hoon Kim <david.jh.kim@berkeley.edu>
2025-10-02 21:57:07 -0300

    updated nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,13 +3,13 @@ require("config.lazy")
 -- Enable syntax highlighting
 vim.cmd("syntax on")
 
--- Colorscheme
-vim.cmd("colorscheme darkblue")
-
 -- Search settings
 vim.opt.ignorecase = true   -- set ic
 vim.opt.incsearch = true    -- set is
 vim.opt.hlsearch = true     -- set hlsearch
+
+-- Colorscheme 
+vim.cmd("colorscheme vim")
 
 -- Line numbers
 vim.opt.number = true
@@ -20,10 +20,13 @@ vim.opt.wildmenu = true
 -- Cursor highlighting
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
-vim.opt.colorcolumn = "80"
 
--- Highlight cursorline colors
-vim.cmd("highlight CursorLine cterm=NONE ctermbg=darkgray guibg=#2e2e2e")
+-- set the highlight color for both line + column
+vim.cmd([[
+  highlight CursorLine   cterm=NONE ctermbg=darkgray guibg=#2e2e2e
+  highlight CursorColumn cterm=NONE ctermbg=darkgray guibg=#2e2e2e
+  highlight CursorLineNr cterm=NONE ctermbg=darkgray guibg=#2e2e2e guifg=#ffffff
+]])
 
 -- Indentation
 vim.opt.tabstop = 2

0711ed132c014ce5da35d344e8557cae57f73b75
DavidKim2003 <gr300158@graded.br>
2026-02-21 22:59:29 -0300

    adding more backgrounds for wezterm

--- wezterm/bg-constants.lua
+++ wezterm/bg-constants.lua
@@ -21,6 +21,21 @@ M.a_snowy_mountain_tops = wezterm.home_dir
 M.a_bridge_with_clouds_in_the_sky = wezterm.home_dir
   .. '/.config/wezterm/assets/a_bridge_with_clouds_in_the_sky.jpg'
 
+M.a_landscape_with_mountains_and_trees = wezterm.home_dir
+  .. '/.config/wezterm/assets/a_landscape_with_mountains_and_trees.jpg'
+
+M.a_dark_alleyway_with_a_brick_building_and_a_window = wezterm.home_dir
+  .. '/.config/wezterm/assets/a_dark_alleyway_with_a_brick_building_and_a_window.jpg'
+
+M.a_fish_swimming_in_water = wezterm.home_dir
+  .. '/.config/wezterm/assets/a_fish_swimming_in_water.png'
+
+M.the_great_wave = wezterm.home_dir
+  .. '/.config/wezterm/assets/the_great_wave.jpg'
+
+M.misaka_pass = wezterm.home_dir
+  .. '/.config/wezterm/assets/misaka_pass.jpg'
+
 -- Changing image
 M.bg_image = M.a_bridge_with_clouds_in_the_sky
 
@@ -36,6 +51,16 @@ elseif M.bg_image == M.bg_foggy_trees then
 elseif M.bg_image == M.a_foggy_landscape_with_trees_and_grass then
   M.brightness = 0.05
 elseif M.bg_image == M.a_bridge_with_clouds_in_the_sky then
+  M.brightness = 0.02
+elseif M.bg_image == M.a_fish_swimming_in_water then 
+  M.brightness = 0.05
+elseif M.bg_image == M.a_dark_alleyway_with_a_brick_building_and_a_window then
+  M.brightness = 0.05
+elseif M.bg_image == M.a_landscape_with_mountains_and_trees then
+  M.brightness = 0.02
+elseif M.bg_image == M.the_great_wave then
+  M.brightness = 0.02
+elseif M.bg_image == M.misaka_pass then
   M.brightness = 0.02
 end
 

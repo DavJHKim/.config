e93596d8cf0ac5f56b7de54e72a75e7a0d2a9642
David Ji Hoon Kim <david.jh.kim@berkeley.edu>
2026-02-22 14:18:38 -0300

    updating oil to show dot files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   "stevearc/oil.nvim",
   opts = {
     default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
     keymaps = {
       ["<Esc>"] = "actions.close",
     },
@@ -10,4 +13,3 @@ return {
     { "-", "<cmd>Oil --float<cr>", mode = "n", desc = "Open Floating Filesystem" },
   },
 }
-

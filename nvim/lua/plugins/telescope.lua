145134fd096a260e2341be241bcd5d7cccd938e7
DavJHKim <djhkim2003@gmail.com>
2026-03-06 09:29:39 -0300

    adjusting telescope window

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,21 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        layout_strategy = "center",
+        layout_config = {
+          width = 0.6,
+          height = 0.4,
+        },
+        border = true,
+        winblend = 10,
+      },
+    })
+  end
 }

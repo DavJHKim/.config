ed51a2233466bc792b6495dfdbb685e1c322e4b3
David Kim <djhkim2003@gmail.com>
2026-05-16 23:14:52 -0300

    refact: adjusting opacity

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -96,7 +96,7 @@ hl.config({
 
         -- Change transparency of focused and unfocused windows
         active_opacity   = 1.0,
-        inactive_opacity = 0.8,
+        inactive_opacity = 1.0,
 
         shadow = {
             enabled      = true,

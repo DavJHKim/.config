52364a64afb7ece1d30903f3659e001ff8a9fc4a
David Ji Hoon Kim <david.jh.kim@berkeley.edu>
2026-05-31 12:23:03 -0300

    feat: adding parsers

--- nvim/lua/plugins/tree-sitter-manager.lua
+++ nvim/lua/plugins/tree-sitter-manager.lua
@@ -4,7 +4,11 @@ return {
   config = function()
     require("tree-sitter-manager").setup({
       -- Default Options
-      -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+       ensure_installed = 
+       { "markdown", "cpp", "python", "c_sharp", "css", "csv", "markdown_inline",
+         "c", "dockerfile", "zsh", "bash", "zig", "lua", "go", "python", "html",
+         "json", "xml", "git_config", "gitignore", "html_tags", "http", "java",
+         "javascript", "vim", "yaml", "hyprlang", "typescript", "git_rebase"},
       -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
       -- auto_install = false, -- if enabled, install missing parsers when editing a new file
       -- highlight = true, -- treesitter highlighting is enabled by default
